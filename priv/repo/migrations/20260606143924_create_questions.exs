bba6afb6ec7ca5b18df54817b51037eda7af31f6
Isaak Tsalicoglou <isaak@waseigo.com>
2026-06-06 17:42:58 +0300

    actually, questions don't need to be user-scoped, since we have no CRUD of questions across polls, and the polls are already user-scoped

--- priv/repo/migrations/20260606143924_create_questions.exs
+++ priv/repo/migrations/20260606143924_create_questions.exs
@@ -4,15 +4,12 @@ defmodule Slidex.Repo.Migrations.CreateQuestions do
   def change do
     create table(:questions, primary_key: false) do
       add :id, :binary_id, primary_key: true
-      add :position, :integer
-      add :body, :string
+      add :position, :integer, default: 0
+      add :body, :string, null: false
       add :poll_id, references(:polls, on_delete: :nothing, type: :binary_id)
-      add :user_id, references(:users, type: :binary_id, on_delete: :delete_all)
 
       timestamps(type: :utc_datetime_usec)
     end
-
-    create index(:questions, [:user_id])
 
     create index(:questions, [:poll_id])
   end
