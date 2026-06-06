8c4cc4af4d4932b929fa75c2415ac9b3243d5d4b
Isaak Tsalicoglou <isaak@waseigo.com>
2026-06-06 15:22:29 +0300

    use Ecto.ULID instead of Ecto.UUID for binary IDs

--- lib/slidex/repo.ex
+++ lib/slidex/repo.ex
@@ -2,4 +2,10 @@ defmodule Slidex.Repo do
   use Ecto.Repo,
     otp_app: :slidex,
     adapter: Ecto.Adapters.Postgres
+
+  @impl true
+  def init(_, opts) do
+    opts = Keyword.put(opts, :uuid, Ecto.ULID)
+    {:ok, opts}
+  end
 end
