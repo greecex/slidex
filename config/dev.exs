66f48421ac40c9b3803c935fcf79b74ca3a6bb93
Isaak Tsalicoglou <isaak@waseigo.com>
2026-06-06 15:23:04 +0300

    repo config will be in runtime.exs, common between dev and prod. Enable IPv6.

--- config/dev.exs
+++ config/dev.exs
@@ -2,13 +2,9 @@ import Config
 
 # Configure your database
 config :slidex, Slidex.Repo,
-  username: "postgres",
-  password: "postgres",
-  hostname: "localhost",
-  database: "slidex_dev",
+  socket_options: [:inet6],
   stacktrace: true,
-  show_sensitive_data_on_connection_error: true,
-  pool_size: 10
+  show_sensitive_data_on_connection_error: true
 
 # For development, we disable any cache and enable
 # debugging and code reloading.
