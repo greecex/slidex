695ccfe52c3ecdaed8bcd84f9facf22500481835
Isaak Tsalicoglou <isaak@waseigo.com>
2026-06-06 18:34:02 +0300

    auto-load user and scope for convenience

--- .iex.exs
+++ .iex.exs
@@ -1,4 +1,25 @@
+require Logger
+
 alias Slidex.Repo
 alias Slidex.Accounts
 alias Slidex.Accounts.User
 alias Slidex.Polling.{Question, Option}
+
+username = "demo"
+
+{user, scope} =
+  case Accounts.get_user_by_email_or_username(username) do
+    %User{} = user ->
+      Logger.debug("variable 'scope' now contains the scope for user #{username}")
+      {user, Accounts.Scope.for_user(user)}
+
+    nil ->
+      Logger.warning("""
+      Could not create scope for user #{username}.
+      No user found with that username.
+      Create the user or run the following in the shell to create the 'demo' user:
+      mix run priv/repo/seeds.exs
+      """)
+
+      {nil, nil}
+  end
