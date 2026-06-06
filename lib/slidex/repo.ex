82c2a809111fdb2b6f59e2ddfd5b1ab843728429
Isaak Tsalicoglou <isaak@waseigo.com>
2026-06-06 19:12:30 +0300

    remove outdated approach for using Ecto.ULID instead of Ecto.UUID

--- lib/slidex/repo.ex
+++ lib/slidex/repo.ex
@@ -2,10 +2,4 @@ defmodule Slidex.Repo do
   use Ecto.Repo,
     otp_app: :slidex,
     adapter: Ecto.Adapters.Postgres
-
-  @impl true
-  def init(_, opts) do
-    opts = Keyword.put(opts, :uuid, Ecto.ULID)
-    {:ok, opts}
-  end
 end
