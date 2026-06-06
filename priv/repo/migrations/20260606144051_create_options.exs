a4d2e674b51ca0da33e8d148dfb073c88f158075
Isaak Tsalicoglou <isaak@waseigo.com>
2026-06-06 17:45:30 +0300

    options don't need to be user-scoped either, plus deleting a question should remove all its options

--- priv/repo/migrations/20260606144051_create_options.exs
+++ priv/repo/migrations/20260606144051_create_options.exs
@@ -7,13 +7,10 @@ defmodule Slidex.Repo.Migrations.CreateOptions do
       add :position, :integer
       add :body, :string
       add :is_correct, :boolean, default: false, null: false
-      add :question_id, references(:questions, on_delete: :nothing, type: :binary_id)
-      add :user_id, references(:users, type: :binary_id, on_delete: :delete_all)
+      add :question_id, references(:questions, on_delete: :delete_all, type: :binary_id)
 
       timestamps(type: :utc_datetime_usec)
     end
-
-    create index(:options, [:user_id])
 
     create index(:options, [:question_id])
   end
