c0c604eee19c0601d3b0dc2062774ca46750892b
Isaak Tsalicoglou <isaak@waseigo.com>
2026-06-06 18:33:47 +0300

    seed demo user for convenience

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,12 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+# Create demo user
+
+%{
+  email: "demo@example.com",
+  username: "demo",
+  confirmed_at: DateTime.utc_now()
+}
+|> Slidex.Accounts.register_user()
