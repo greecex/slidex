ec1167235d2d4ac28a08bd6205484dabc73e8e1e
Isaak Tsalicoglou <isaak@waseigo.com>
2026-06-06 15:36:33 +0300

    `mix phx.gen.auth Accounts User users --hashing-lib argon2`

--- config/config.exs
+++ config/config.exs
@@ -7,6 +7,19 @@
 # General application configuration
 import Config
 
+config :slidex, :scopes,
+  user: [
+    default: true,
+    module: Slidex.Accounts.Scope,
+    assign_key: :current_scope,
+    access_path: [:user, :id],
+    schema_key: :user_id,
+    schema_type: :binary_id,
+    schema_table: :users,
+    test_data_fixture: Slidex.AccountsFixtures,
+    test_setup_helper: :register_and_log_in_user
+  ]
+
 config :slidex,
   ecto_repos: [Slidex.Repo],
   generators: [
