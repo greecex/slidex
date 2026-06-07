46c701b02888cde7a5b8f060b5ee6ee396000bb4
Isaak Tsalicoglou <isaak@waseigo.com>
2026-06-07 09:12:17 +0300

    added exclusion logic when searching, plus preloads helper

--- lib/slidex/polling.ex
+++ lib/slidex/polling.ex
@@ -34,21 +34,7 @@ defmodule Slidex.Polling do
     %Question{poll_id: poll.id}
     |> Question.changeset(attrs)
     |> Repo.insert()
-  end
-
-  def search_question_bodies(%Scope{} = scope, search_term, opts \\ []) do
-    limit = Keyword.get(opts, :limit, 10)
-
-    Question
-    |> join(:inner, [q], p in assoc(q, :poll))
-    |> where([q, p], p.user_id == ^scope.user.id)
-    |> where([q], ilike(q.body, ^"%#{search_term}%"))
-    |> order_by([q], asc: q.body)
-    |> limit(^limit)
-    # |> select([q], %{id: q.id, body: q.body})
-    |> distinct([q], q.body)
-    |> select([q], q.body)
-    |> Repo.all()
+    |> with_preloads()
   end
 
   # TODO: Add check to prevent editing if the question already has responses
@@ -59,6 +45,7 @@ defmodule Slidex.Polling do
     question
     |> Question.changeset(attrs)
     |> Repo.update()
+    |> with_preloads()
   end
 
   # TODO: Add check to prevent deletion if the question already has responses
@@ -96,6 +83,7 @@ defmodule Slidex.Polling do
     %Option{question_id: question.id}
     |> Option.changeset(attrs)
     |> Repo.insert()
+    |> with_preloads()
   end
 
   # TODO: Add check to prevent editing if the question already has responses
@@ -106,6 +94,7 @@ defmodule Slidex.Polling do
     option
     |> Option.changeset(attrs)
     |> Repo.update()
+    |> with_preloads()
   end
 
   # TODO: Add check to prevent deletion if the question already has responses
@@ -115,4 +104,76 @@ defmodule Slidex.Polling do
 
     Repo.delete(option)
   end
+
+  # Search functionality with optional exclusion
+
+  def search_question_bodies(%Scope{} = scope, search_term, opts \\ []) do
+    limit = Keyword.get(opts, :limit, 10)
+    excluded = Keyword.get(opts, :excluded, [])
+
+    from(Question, as: :q)
+    |> join(:inner, [q], p in assoc(q, :poll), as: :p)
+    |> where([p: p], p.user_id == ^scope.user.id)
+    |> where([q: q], ilike(q.body, ^"%#{search_term}%"))
+    |> maybe_exclude_many(excluded)
+    |> order_by([q: q], asc: q.body)
+    |> limit(^limit)
+    |> distinct([q: q], q.body)
+    |> select([q: q], q.body)
+    |> Repo.all()
+  end
+
+  def search_option_bodies(%Scope{} = scope, search_term, opts \\ []) do
+    limit = Keyword.get(opts, :limit, 10)
+    excluded = Keyword.get(opts, :excluded, [])
+
+    from(Option, as: :o)
+    |> join(:inner, [o: o], q in assoc(o, :question), as: :q)
+    |> join(:inner, [q: q], p in assoc(q, :poll), as: :p)
+    |> where([p: p], p.user_id == ^scope.user.id)
+    |> where([o: o], ilike(o.body, ^"%#{search_term}%"))
+    |> maybe_exclude_many(excluded)
+    |> order_by([o: o], asc: o.body)
+    |> limit(^limit)
+    |> distinct([o: o], o.body)
+    |> select([o: o], o.body)
+    |> Repo.all()
+  end
+
+  # Exclusion query helpers for searching across questions and options
+
+  defp maybe_exclude_many(query, list) when is_list(list) do
+    Enum.reduce(list, query, fn x, acc -> maybe_exclude_one(acc, x) end)
+  end
+
+  defp maybe_exclude_many(query, _), do: query
+
+  defp maybe_exclude_one(query, %Poll{id: poll_id}) do
+    query
+    |> where([p: p], p.id != ^poll_id)
+  end
+
+  defp maybe_exclude_one(query, %Question{id: question_id}) do
+    query
+    |> where([q: q], q.id != ^question_id)
+  end
+
+  defp maybe_exclude_one(query, %Option{id: option_id}) do
+    query
+    |> where([o: o], o.id != ^option_id)
+  end
+
+  defp maybe_exclude_one(query, _), do: query
+
+  # Preloads
+
+  def with_preloads({:ok, %Question{} = q}) do
+    {:ok, Repo.preload(q, [:poll, :options])}
+  end
+
+  def with_preloads({:ok, %Option{} = q}) do
+    {:ok, Repo.preload(q, question: [:poll])}
+  end
+
+  def with_preloads({:error, _} = e), do: e
 end
