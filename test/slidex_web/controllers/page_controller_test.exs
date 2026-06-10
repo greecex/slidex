bbe562234ee93b2568a30f47b71b3ee5ea5c41e3
Petros Amoiridis <petros@hey.com>
2026-06-11 00:04:09 +0300

    Fix home page controller test
    Assert the actual landing copy instead of the default Phoenix template text.

--- test/slidex_web/controllers/page_controller_test.exs
+++ test/slidex_web/controllers/page_controller_test.exs
@@ -3,6 +3,6 @@ defmodule SlidexWeb.PageControllerTest do
 
   test "GET /", %{conn: conn} do
     conn = get(conn, ~p"/")
-    assert html_response(conn, 200) =~ "Peace of mind from prototype to production"
+    assert html_response(conn, 200) =~ "Live polls with Phoenix LiveView."
   end
 end
